4351c198e6c45c21e1707f10eedaf0f54b64c724
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-24 12:12:23 +0000

    MyTestOfRandoms: added a dice generator

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -7,17 +7,19 @@ namespace MyTestOfRandoms
         static void Main(string[] args)
         {
             //variables
-            int n, dado;
+            int n, result;
+            Random dado = new Random();
 
             //ask user for n
             Console.Write("Write a number of dice to be rolled: ");
             n = int.Parse(Console.ReadLine());
 
             //dice generator
+            result = dado.Next(1, 7);
             //add n dices
             //show result
-            Console.WriteLine($"The result of rolling {n} dice is: {0}");
-            
+            Console.WriteLine($"The result of rolling {n} dice is: {result}");
+
         }
     }
 }
