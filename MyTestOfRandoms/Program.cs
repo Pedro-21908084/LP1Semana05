dd3c532e92b6d83ce81f3a171e65c745e4e5032c
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-24 12:08:38 +0000

    MyTestOfRandoms: read user input

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -6,7 +6,18 @@ namespace MyTestOfRandoms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //variables
+            int n, dado;
+
+            //ask user for n
+            Console.Write("Write a number of dice to be rolled: ");
+            n = int.Parse(Console.ReadLine());
+
+            //dice generator
+            //add n dices
+            //show result
+            Console.WriteLine($"The result of rolling {n} dice is: {0}");
+            
         }
     }
 }
