8cc42c682728ffb00d844bd2e4e651082b0bfb50
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-24 12:15:56 +0000

    MyTestOfRandoms: made it rolle n dice and add everyting

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -7,16 +7,20 @@ namespace MyTestOfRandoms
         static void Main(string[] args)
         {
             //variables
-            int n, result;
+            int n, result = 0;
             Random dado = new Random();
 
             //ask user for n
             Console.Write("Write a number of dice to be rolled: ");
             n = int.Parse(Console.ReadLine());
 
-            //dice generator
-            result = dado.Next(1, 7);
             //add n dices
+            for (int i = 0; i < n; i++)
+            {
+                //dice generator
+                result += dado.Next(1, 7);
+            }
+
             //show result
             Console.WriteLine($"The result of rolling {n} dice is: {result}");
 
